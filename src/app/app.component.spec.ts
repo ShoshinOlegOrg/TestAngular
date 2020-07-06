1281cdad06da9a7ac7a3bf2017f6401bc16b10e2
ShoshinOleg <shoshin1997@rambler.ru>
2020-07-06 18:08:04 +0300

    First working project.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'TestAngular'`, () => {
+  it(`should have as title 'Project-Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('TestAngular');
+    expect(app.title).toEqual('Project-Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('TestAngular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Project-Angular app is running!');
   });
 });
